8ceec28c389bcd1049b24da79ca6cda6eff30ac4
sam <thambad@gmail.com>
2024-12-12 19:44:21 +0800

    added sidebar into layout

---
+++ app/login/page.tsx
@@ -0,0 +1,8 @@
+import TextField from '@mui/material/TextField';
+export default function Login() {
+  return (
+    <div>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </div>
+  );
+}
